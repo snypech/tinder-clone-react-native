1a8304951c1a2e354e78e9fd542089cd041ce268
snypech <sneyder.yepes@gmail.com>
2021-11-25 14:43:11 -0500

    imageback for login, remove header, touchable op for login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/core';
+import React, {useLayoutEffect} from 'react'
+import { View, Text, Button, ImageBackground, TouchableOpacity } from 'react-native'
 import useAuth from '../hooks/useAuth'
+import tw from'tailwind-rn';
 
 const LoginScreen = () => {
     const {loading,signInWithGoogle} = useAuth();
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown:false
+        })
+    }, [])
     return (
-        <View>
-            <Text>{loading ? 'loading...' : 'Login to the app'}</Text>
-            <Button title='login' onPress={signInWithGoogle}/>
+        <View style={tw('flex-1')}>
+            <ImageBackground
+            source={{uri:'https://tinder.com/static/tinder.png'}}
+            style={tw('flex-1 items-center')}
+            resizeMode='cover'
+            >
+                <TouchableOpacity style={[tw('bg-white rounded-full w-52 p-4'),{top:"75%"}]}
+                onPress={signInWithGoogle}
+                >
+                    {/* <Text>{loading ? 'loading...' : 'Login to the app'}</Text> */}
+                    <Text style={[tw('font-semibold text-center')]}>Sign in & get swiping</Text>
+                </TouchableOpacity>
+                
+                {/* <Button title='login' onPress={signInWithGoogle}/>  */}
+            </ImageBackground>
+            
         </View>
     )
 }
