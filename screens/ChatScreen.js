df728b6591d043f57d848136d440740ff819fa01
snypech <sneyder.yepes@gmail.com>
2021-11-27 21:40:34 -0500

    show swiping cards by installing react native deck swiper, swiper inside view flex 1 style container style

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const ChatScreen = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Im the chat screen</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,12 +5,37 @@ import useAuth from '../hooks/useAuth';
 import tw from'tailwind-rn';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {Ionicons} from'@expo/vector-icons'
+import Swiper from 'react-native-deck-swiper';
+
+const burned_data=[
+    {
+        firstName:'Elizabeth',
+        lastName:'Olsen',
+        occupation:'Actress',
+        photoURL:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2NTE5MzAyNjY3/elizabeth-olsen-20631899-1-402.jpg',
+        age:32
+    },
+    {
+        firstName:'Elon',
+        lastName:'Musk',
+        occupation:'Software Developer',
+        photoURL:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg',
+        age:40
+    },
+    {
+        firstName:'Rafael',
+        lastName:'Nadal',
+        occupation:'Tennis player',
+        photoURL:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5ODc2ODQzNzY2MTYzMDU1/gettyimages-982701222.jpg',
+        age:35
+    }
+]
 
 const HomeScreen = () => {
     const navigation = useNavigation();
     const {user, logout}=useAuth()
     return (
-        <SafeAreaView>
+        <SafeAreaView style={tw('flex-1')}>
             <View style={tw('flex-row items-center justify-between px-4')}>
                 <TouchableOpacity>
                     <Image 
@@ -27,12 +52,35 @@ const HomeScreen = () => {
                     />
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Ionicons name='chatbubble-sharp' size={30} color='#FF5864'/>
+                    <Ionicons name='chatbubble-sharp' size={30} color='#FF5864' onPress={()=>navigation.navigate('ChatScreen')}/>
                 </TouchableOpacity>
             </View>
-            <Text>Im the homescreen</Text>
-            <Button title="Go to the chat screen" onPress={()=>navigation.navigate('ChatScreen')}/>
-            <Button title="Logout" onPress={()=>logout()}/>
+            <View style={tw('flex-1')}>
+                <Swiper 
+                containerStyle={tw('bg-transparent')} cards={burned_data} 
+                stackSize={5}
+                cardIndex={0}
+                animateCardOpacity
+                verticalSwipe={false}
+                renderCard={
+                    (card)=>(
+                        <View style={tw('bg-white h-3/4 rounded-xl')}>
+                            <Image style={tw('flex-1 rounded-t-xl')} source={{uri:card.photoURL}}/> 
+                            <View style={tw('flex-row justify-between p-4')}>
+                                <View>
+                                    <Text style={tw('text-xl font-bold')}>{card.firstName} {card.lastName}</Text>
+                                    <Text>{card.occupation}</Text>
+                                </View>
+                                <Text style={tw('text-2xl font-bold')}>{card.age}</Text>
+                            </View>
+                            {/* <Text>{card.firstName}</Text> */}
+                        </View>
+                    )
+                }
+                />
+            </View>
+            {/* <Button title="Go to the chat screen" onPress={()=>navigation.navigate('ChatScreen')}/>
+            <Button title="Logout" onPress={()=>logout()}/> */}
         </SafeAreaView>
     )
 }
